09652e4c7903b766edb813eae0bdb73b0381015e
David Tolnay <dtolnay@gmail.com>
2019-07-20 13:18:43 -0700

    Remove clone of the fst data

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-use fst::Set;
+use fst::raw::Fst;
 use lazy_static::lazy_static;
 use wasm_bindgen::prelude::*;
 
 const FST: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/password_hash.fst"));
 
 lazy_static! {
-    static ref PASSWORDS: Set = Set::from_bytes(FST.to_owned()).unwrap();
+    static ref PASSWORDS: Fst = Fst::from_static_slice(FST).unwrap();
 }
 
 #[wasm_bindgen]
 pub fn is_compromised(test_password: &str) -> bool {
-    PASSWORDS.contains(test_password)
+    PASSWORDS.contains_key(test_password)
 }
 
 #[cfg(test)]
